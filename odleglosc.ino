210f71ad4ebcc424dd9529628207fc6d97b952f3
Michał Sadowski SQ6JX <sq6jnx@hamradio.pl>
2012-08-26 22:17:13 +0200

    proper APRS-telemetry generation.
    work in progress
    
    Signed-off-by: Michał Sadowski SQ6JNX <sq6jnx@hamradio.pl>

--- odleglosc.ino
+++ odleglosc.ino
@@ -1,7 +1,5 @@
 // vim: syn=cpp tabw=2
 
-#define trigPin 9
-#define echoPin 10
 
 
 
@@ -21,8 +19,8 @@
 #include <avr/sleep.h>
 
 void setup() {
- pinMode(trigPin,OUTPUT);       // set TP output pin for trigger
- pinMode(echoPin,INPUT);        // set EP input pin for echo
+ pinMode(SONAR_TRIGGER_PIN,OUTPUT);  // set TP output pin for trigger
+ pinMode(SONAR_ECHO_PIN,INPUT);      // set EP input pin for echo
 
 
   //pinMode(LED_PIN, OUTPUT);                                                     
@@ -32,18 +30,16 @@ void setup() {
 #endif                                                                          
    modem_setup();                                                                
    //buzzer_setup();                                                               
-  pinMode(echoPin, INPUT);
+  pinMode(SONAR_ECHO_PIN, INPUT);
            
 }
-
-
 
 //#include <stdio.h>
 //#include <stdlib.h>
 
-// Exported functions // taken from aprs.cpp and modified by SQ6JNX
-void aprs_send()
-{
+
+void telemetry_report(int n, 
+        double a1=0,double a2=0,double a3=0,double a4=0,double a5=0) {
   const struct s_address addresses[] = { 
     {D_CALLSIGN, D_CALLSIGN_ID},  // Destination callsign
     {S_CALLSIGN, S_CALLSIGN_ID},  // Source callsign (-11 = balloon, -9 = car)
@@ -55,41 +51,7 @@ void aprs_send()
 #endif
   };
 
-  ax25_send_header(addresses, sizeof(addresses)/sizeof(s_address));
-  // according to http://www.aprs.pl/teoria.htm#ident we should use 'T' as
-  // telemetry or '!' as 'Position without timestamp (no APRS messaging), 
-  // or Ultimeter 2000 WX Station'
-
-  // example frame is:
-  // !5225.85NS01654.50E#
-  // where:
-  // ! - identifier
-  // 5225.85N - szerokość geograficzna
-  // 01654.50E - długość geograficzna
-  // S# - symbol
-  ax25_send_byte('T');
-  ax25_send_string("5107.76N");     // Lat: 38deg and 22.20 min (.20 are NOT seconds, but 1/100th of minutes)
-  ax25_send_byte('S');              // Symbol table
-  ax25_send_string("1654.26");      // Lon: 000deg and 25.80 min
-  ax25_send_byte('#');              // Symbol: O=balloon, -=QTH
-  ax25_send_string("Vin=8.8V");     // input voltage
-  ax25_send_string("H=80/Hw=120/Ha=150/");     // sensor values
-  ax25_send_string(APRS_COMMENT);     // Comment
-  ax25_send_footer();
-  ax25_flush_frame();                 // Tell the modem to go
-}
-
-void telemetry_report_format() {
-  const struct s_address addresses[] = { 
-    {D_CALLSIGN, D_CALLSIGN_ID},  // Destination callsign
-    {S_CALLSIGN, S_CALLSIGN_ID},  // Source callsign (-11 = balloon, -9 = car)
-#ifdef DIGI_PATH1
-    {DIGI_PATH1, DIGI_PATH1_TTL}, // Digi1 (first digi in the chain)
-#endif
-#ifdef DIGI_PATH2
-    {DIGI_PATH2, DIGI_PATH2_TTL}, // Digi2 (second digi in the chain)
-#endif
-  };
+  char buffer[33];
 
   ax25_send_header(addresses, sizeof(addresses)/sizeof(s_address));
   ax25_send_byte('T');            // Telemetry symbol
@@ -97,152 +59,114 @@ void telemetry_report_format() {
 
   ax25_send_string("#MIC");       // MIC-E format (or, maybe, serial number??)
 
-  ax25_send_string("255");         // analog value 1 (3 digits)
-  ax25_send_byte(',');
-  ax25_send_string("255");         // analog value 2 (3 digits)
-  ax25_send_byte(',');
-  ax25_send_string("255");         // analog value 3 (3 digits)
-  ax25_send_byte(',');
-  ax25_send_string("255");         // analog value 4 (3 digits)
-  ax25_send_byte(',');
-  ax25_send_string("255");         // analog value 5 (3 digits)
+  
+  itoa( (int)(a1-A1c)/A1b,buffer,10);
+  ax25_send_string(buffer);         // analog value 1 (3 digits)
+  
+  if(n>1) {
+      itoa( (int)(a2-A2c)/A2b,buffer,10);
+      ax25_send_byte(',');
+      ax25_send_string("255");         // analog value 2 (3 digits)
+  }
+
+  if(n>2) {
+      itoa( (int)(a3-A3c)/A3b,buffer,10);
+      ax25_send_byte(',');
+      ax25_send_string("255");         // analog value 3 (3 digits)
+  }
+
+  if(n>3) {
+      itoa( (int)(a4-A4c)/A4b,buffer,10);
+      ax25_send_byte(',');
+      ax25_send_string("255");         // analog value 4 (3 digits)
+  }
+
+  if(n>4) {
+      itoa( (int)(a5-A5c)/A5b,buffer,10);
+      ax25_send_byte(',');
+      ax25_send_string("255");         // analog value 5 (3 digits)
+  }
 
   // I'll leave it here for future generations.
   //ax25_send_byte(',');
   //ax25_send_string("00000000");      // digital value (8 0/1 digits)
   //ax25_send_byte(',');
-  ax25_send_string("comment");       // comment
+
+  ax25_send_string(OBJECT_COMMENT);       // comment
   ax25_send_footer();
   ax25_flush_frame();                 // Tell the modem to go
 }
 
 
-void parameter_name_message() {
+ //Exported functions // taken from aprs.cpp and modified by SQ6JNX
+void aprs_send(int poziom_wody)
+{
   const struct s_address addresses[] = { 
     {D_CALLSIGN, D_CALLSIGN_ID},  // Destination callsign
     {S_CALLSIGN, S_CALLSIGN_ID},  // Source callsign (-11 = balloon, -9 = car)
-#ifdef DIGI_PATH1
+/*#ifdef DIGI_PATH1
     {DIGI_PATH1, DIGI_PATH1_TTL}, // Digi1 (first digi in the chain)
 #endif
 #ifdef DIGI_PATH2
     {DIGI_PATH2, DIGI_PATH2_TTL}, // Digi2 (second digi in the chain)
-#endif
+#endif*/
   };
 
-  ax25_send_header(addresses, sizeof(addresses)/sizeof(s_address));
-  ax25_send_string("PARM.");
-  ax25_send_string("Stan");       // Analog name 1 (1-6 letters)
-  ax25_send_byte(',');            
-  ax25_send_string("Ostrz");      // Analog name 2 (1-6 letters)
-  ax25_send_byte(',');            
-  ax25_send_string("Alar");       // Analog name 3 (1-5 letters)
-  ax25_send_byte(',');            
-  ax25_send_string("Vin");        // Analog name 4 (1-5 letters)
-  // left here for future generations
-  //ax25_send_byte(',');            
-  //ax25_send_string("000");        // Analog name 5 (1-4 letters)
-  //ax25_send_byte(',');            
-  //ax25_send_string("0");          // Binary name 1 (1-5 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 2 (1-5 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 3 (1-3 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 4 (1-3 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 5 (1-3 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 6 (1-2 letters)
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 7 (1-2 letters)
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 8 (1-2 letters)
-  ax25_send_footer();
-  ax25_flush_frame();                 // Tell the modem to go
-}
-
-void unit_label_message() {
-  const struct s_address addresses[] = { 
-    {D_CALLSIGN, D_CALLSIGN_ID},  // Destination callsign
-    {S_CALLSIGN, S_CALLSIGN_ID},  // Source callsign (-11 = balloon, -9 = car)
-#ifdef DIGI_PATH1
-    {DIGI_PATH1, DIGI_PATH1_TTL}, // Digi1 (first digi in the chain)
-#endif
-#ifdef DIGI_PATH2
-    {DIGI_PATH2, DIGI_PATH2_TTL}, // Digi2 (second digi in the chain)
-#endif
-  };
+  char symbol[3]="/w";
+  char buffer[33];
+  if(poziom_wody>WATER_WARN_STATE) {
+      strcpy(symbol,"\\w");
+  } else if (poziom_wody<0) {
+        strcpy(buffer, "ERR");
+  } else {
+      itoa(poziom_wody,buffer,10);
+  }
 
   ax25_send_header(addresses, sizeof(addresses)/sizeof(s_address));
-  ax25_send_string("UNIT.");
-  ax25_send_string("cm");       // Analog name 1 (1-6 letters)
-  ax25_send_byte(',');            
-  ax25_send_string("cm");      // Analog name 2 (1-6 letters)
-  ax25_send_byte(',');            
-  ax25_send_string("cm");       // Analog name 3 (1-5 letters)
-  ax25_send_byte(',');            
-  ax25_send_string("V");        // Analog name 4 (1-5 letters)
-  // left here for future generations
-  //ax25_send_byte(',');            
-  //ax25_send_string("000");        // Analog name 5 (1-4 letters)
-  //ax25_send_byte(',');            
-  //ax25_send_string("0");          // Binary name 1 (1-5 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 2 (1-5 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 3 (1-3 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 4 (1-3 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 5 (1-3 letters)                                  
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 6 (1-2 letters)
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 7 (1-2 letters)
-  //ax25_send_byte(',');                                                                              
-  //ax25_send_string("0");          // Binary name 8 (1-2 letters)
+  ax25_send_byte(')');                 // Item report format
+  ax25_send_string(OBJECT_NAME);       // Item name
+  ax25_send_byte('!');                 // Live Item
+  ax25_send_string(OBJECT_LATITUDE);   // Lat: 38deg and 22.20 min (.20 are 
+                                       // NOT seconds, but 1/100th of minutes)
+  ax25_send_byte(symbol[0]);                 // Symbol table ID
+  ax25_send_string(OBJECT_LONGITUDE);  // Lon: 000deg and 25.80 min
+  ax25_send_byte(symbol[1]);                 // Symbol code (w==water station)
+  // comment:  36-43 chars
+  ax25_send_string(OBJECT_COMMENT);
+  ax25_send_string(buffer);
+  ax25_send_string("/140/180cm 5.7V");   
+
   ax25_send_footer();
-  ax25_flush_frame();                 // Tell the modem to go
+  ax25_flush_frame();              // Tell the modem to go
 }
+
+double get_distance() {
+  int duration, distance;
+  digitalWrite(SONAR_TRIGGER_PIN, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(SONAR_TRIGGER_PIN, LOW);
+  duration = pulseIn(SONAR_ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance >= 500 || distance <= 0){
+      return -1;
+  }
+  else {
+      Serial.print(distance);
+      Serial.println(" cm");
+      return distance;
+  }
+}  
+
 
 void loop() {
-  
-  /* 
-  
-  We must implement two -- overlapping -- "loops":
 
-  First one takes, say 10 measurements in 1 minute intervals, then takes an
-  average. This average is sensor value we're going to push via APRS.
+    double sensor = get_distance();
+    if (sensor<0) {
+        aprs_send(-1);
+    } else {
+        double current_state = WATER_MOUNT_HEIGHT-sensor;
+        aprs_send((int)current_state);
+    }
 
-  The second loop has to send APRS frames with:
-  * Telemetry Report Format
-  * Parameter Name Message
-  * Unit/Label Message
-  * Equation Coefficients Message
-  * Bit Sense/Project Name Message
-
-  Thus, we can measure water level after every frame.
-
-  */
-
-
-
-
-    
-  int duration, distance;
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  //if (distance >= 500 || distance <= 0){
-  //  Serial.println("Out of range");
-  //}
-  //else {
-    Serial.print(distance);
-    Serial.println(" cm");
-  //}
-  delay(500);
-  aprs_send();
-  delay(5000);
+    delay(6000);
 }
